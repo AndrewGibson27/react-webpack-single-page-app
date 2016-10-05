a4f7667c0bd1f95d5cebf8af967c859e71fe0fac
Andrew Gibson <atgibson27@gmail.com>
2016-10-04 22:22:58 -0400

    build modernizr with gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
 var critical = require('critical').stream;
+var modernizr = require('gulp-modernizr');
+var saveLicense = require('uglify-save-license');
+var uglify = require('gulp-uglify');
+
+gulp.task('modernizr', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(modernizr({
+      options: [],
+      tests: ['csscalc', 'svg', 'flexbox'],
+      crawl: false
+    }))
+    .pipe(uglify({
+      output: {
+        comments: saveLicense
+      }
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
 
 gulp.task('critical', function () {
   return gulp.src('dist/index.html')
@@ -13,4 +31,4 @@ gulp.task('critical', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['critical']);
+gulp.task('default', ['critical', 'modernizr']);
