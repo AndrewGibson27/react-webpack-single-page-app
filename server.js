b40bc37b3b505b4b96292f22a7a19344f776d562
Andrew Gibson <atgibson27@gmail.com>
2016-10-04 22:46:22 -0400

    fix indentation

--- server.js
+++ server.js
@@ -9,21 +9,22 @@ var compiler = webpack(config);
 app.use(express.static('dist'));
 
 app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(3000, 'localhost', function(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log(err);
+    
+    return false;
+  }
 
-    console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://localhost:3000');
 });
