a5c30e0009d48f33b1c72e49ddbbd8b62cdf007b
Andrew Gibson <atgibson27@gmail.com>
2016-10-18 21:52:35 -0400

    reset cache

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
+var pug = require('pug');
 
 var app = express();
 var compiler = webpack(config);
