ccf4343ae9061141b2ba79a858d25b716673e270
Andrew Gibson <atgibson27@gmail.com>
2017-01-05 23:44:07 -0600

    make everything a bit cleaner

--- src/components/App.js
+++ src/components/App.js
@@ -24,19 +24,23 @@ export default class App extends Component {
     let class_name;
     
     if (!this.state.mounted) {
-      class_name = 'unmounted';
+      class_name = 'paragraph--unmounted';
     } else {
-      class_name = 'mounted';
+      class_name = 'paragraph--mounted';
     }
   
     return (
       <div>
         <p className={class_name}>Hello world. My color change is controlled by CSS modules, and my media query size change is controlled by a Sass mixin.</p>
-        <img src='img/placeholder.png' />
-        <p>Above me is an image that is required in a React component.</p>
+        <img className='image' src='img/placeholder.png' />
+        <p className='image-caption'>Above me is an image that is required in a React component.</p>
         <p className='font_example'>My font is loaded by webfontloader.</p>
-        <div className='clearfix'>I am a floated div that uses a clearfix mixin.</div>
+        <div className='float'>
+          <div className='float__item'>Demonstrates @clearfix mixin</div>
+          <div className='float__item'></div>
+        </div>
         <div className='twitter'></div>
+        <p className='twitter-caption'>Above me is a sprite image handled by PostCSS</p>
       </div>
     );
   }
