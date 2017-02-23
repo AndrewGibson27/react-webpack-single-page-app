f4c9cfdabea67f74e655b0f368664daa7efe4754
Andrew Gibson <atgibson27@gmail.com>
2017-02-22 23:25:09 -0600

    add trailing slash to publicPath

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist', 'build'),
     filename: 'bundle-build.js',
-    publicPath: '/build'
+    publicPath: '/build/'
   },
 
   plugins: [
